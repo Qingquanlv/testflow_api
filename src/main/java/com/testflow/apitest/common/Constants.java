77523d26d14d9f2a7f5b691fe11c12896ad5f5ed
Qingquan Lv <qingquanlv@gmail.com>
2019-07-07 18:35:07 +0800

    Update Constants.java

--- src/main/java/com/testflow/apitest/common/Constants.java
+++ src/main/java/com/testflow/apitest/common/Constants.java
@@ -6,6 +6,19 @@ public class Constants {
     public static final String MAPVALUE = "value";
     public static final String COLON = ":";
 
-    public static final String SERVICES_PATH = "com.testflow.apitest.servicesaccess.";
-    public static final String PARSE_VALUE_FILE_NAME = "ParseValueFileName.jar";
+    public static final String SERVICES_CLASS_PATH = "com.testflow.apitest.servicesaccess.ParseValueFileName";
+    public static final String PARSE_VALUE_FILE_NAME = "ParseValueFileName.java";
+    public static final String PARSE_VALUE_FILE_SOURCE = "package com.testflow.apitest.servicesaccess;" +
+            "" +
+            "import paramType1;" +
+            "import paramType2;" +
+            "\n" +
+            "public class ParseValueFileName " +
+            "{    \n" +
+            "    method" +
+            "}";
+    public static final String PARAMTYPE1 = "paramType1";
+    public static final String PARAMTYPE2 = "paramType2";
+    public static final String METHOD = "method";
+
 }
