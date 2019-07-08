a6a5127f52c773f8240fe36df21c86d986762516
Qingquan Lv <qingquanlv@gmail.com>
2019-07-08 14:09:41 +0800

    Update Request.java

--- src/main/java/com/testflow/apitest/stepdefinations/Request.java
+++ src/main/java/com/testflow/apitest/stepdefinations/Request.java
@@ -15,15 +15,19 @@ import java.util.Map;
  */
 public class Request {
 
+    private static Logger logger = LoggerFactory.getLogger(Request.class);
     /**
      * 发送Json请求
      *
      */
     public void sendRequest(String requestJsonStr, String url, String responce) {
         requestJsonStr = ParamUtil.parseParam(requestJsonStr);
+        logger.info(String.format("Send request: %s", requestJsonStr));
         url = ParamUtil.parseParam(url);
+        logger.info(String.format("Url: %s", url));
         //保存Response到Buffer
         String requestObject = HttpClientUtil.sendHttpPostJson(url, requestJsonStr);
+        logger.info(String.format("Get responce: %s", requestObject));
         BufferManager.addBufferByKey(responce, requestObject);
     }
 
@@ -34,7 +38,11 @@ public class Request {
     public void sendRequest(String requsetName, List<Map<String, String>> requestListMap, String url, String responce) {
         String requestJsonStr = TableUtil.convertListMapToJSON(requsetName,  requestListMap);
         //保存Response到Buffer
+        logger.info(String.format("Send request: %s", requestJsonStr));
+        url = ParamUtil.parseParam(url);
+        logger.info(String.format("Url: %s", url));
         String requestObject = HttpClientUtil.sendHttpPostJson(url, requestJsonStr);
+        logger.info(String.format("Get responce: %s", requestObject));
         BufferManager.addBufferByKey(responce, requestObject);
     }
 }
