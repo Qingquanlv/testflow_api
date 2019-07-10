fb28d42e51d55b589ba39bc2ce42f0f829dcac4b
Qingquan Lv <qingquanlv@gmail.com>
2019-07-10 19:26:13 +0800

    Update Request.java

--- src/main/java/com/testflow/apitest/stepdefinations/Request.java
+++ src/main/java/com/testflow/apitest/stepdefinations/Request.java
@@ -4,7 +4,10 @@ import com.testflow.apitest.business.BufferManager;
 import com.testflow.apitest.servicesaccess.HttpClientUtil;
 import com.testflow.apitest.utilities.ParamUtil;
 import com.testflow.apitest.utilities.TableUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -22,12 +25,12 @@ public class Request {
      */
     public void sendRequest(String requestJsonStr, String url, String responce) {
         requestJsonStr = ParamUtil.parseParam(requestJsonStr);
-        logger.info(String.format("Send request: %s", requestJsonStr));
+        logger.info(String.format("%s Send request: %s", new Date(), requestJsonStr));
         url = ParamUtil.parseParam(url);
-        logger.info(String.format("Url: %s", url));
+        logger.info(String.format("%s Url: %s", new Date(), url));
         //保存Response到Buffer
         String requestObject = HttpClientUtil.sendHttpPostJson(url, requestJsonStr);
-        logger.info(String.format("Get responce: %s", requestObject));
+        logger.info(String.format("%s Get responce: %s", new Date(), requestObject));
         BufferManager.addBufferByKey(responce, requestObject);
     }
 
@@ -38,13 +41,11 @@ public class Request {
     public void sendRequest(String requsetName, List<Map<String, String>> requestListMap, String url, String responce) {
         String requestJsonStr = TableUtil.convertListMapToJSON(requsetName,  requestListMap);
         //保存Response到Buffer
-        logger.info(String.format("Send request: %s", requestJsonStr));
+        logger.info(String.format("%s Send request: %s", new Date(), requestJsonStr));
         url = ParamUtil.parseParam(url);
-        logger.info(String.format("Url: %s", url));
+        logger.info(String.format("%s Url: %s", new Date(), url));
         String requestObject = HttpClientUtil.sendHttpPostJson(url, requestJsonStr);
-        logger.info(String.format("Get responce: %s", requestObject));
+        logger.info(String.format("%s Get responce: %s", new Date(), requestObject));
         BufferManager.addBufferByKey(responce, requestObject);
     }
 }
-
-
