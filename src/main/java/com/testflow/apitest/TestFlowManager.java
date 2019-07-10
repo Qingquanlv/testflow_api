ae0268b12c6f5b1057e322ff9b7711f49d8f0e64
Qingquan Lv <qingquanlv@gmail.com>
2019-07-10 23:26:22 +0800

    Update TestFlowManager.java

--- src/main/java/com/testflow/apitest/TestFlowManager.java
+++ src/main/java/com/testflow/apitest/TestFlowManager.java
@@ -1,3 +1,5 @@
+package com.testflow.apitest;
+
 import com.testflow.apitest.business.BufferManager;
 import com.testflow.apitest.parser.DataParser;
 import com.testflow.apitest.servicesaccess.ServiceAccess;
@@ -31,6 +33,9 @@ public class TestFlowManager {
 
         /**
          * 发送Json请求
+         * @param requestJsonStr : Json格式请求
+         * @param url : 请求url
+         * @param responce : 保存response的key
          *
          */
         public Runner sendRequest(String requestJsonStr, String url, String responce) {
@@ -39,6 +44,14 @@ public class TestFlowManager {
             return this;
         }
 
+        /**
+         * 发送Cucumber格式请求
+         * @param requsetName : cucumber格式RequestName
+         * @param requestListMap : cucumber table.asMaps 格式request参数
+         * @param url : 请求url
+         * @param responce : 保存response的key
+         *
+         */
         public Runner sendRequest(String requsetName, List<Map<String, String>> requestListMap, String url, String responce) {
             Request request = new Request();
             request.sendRequest(requsetName, requestListMap, url, responce);
@@ -46,13 +59,19 @@ public class TestFlowManager {
         }
 
         /**
-         * 转化方法(String参数)
+         * 使用String格式函数Parse
+         * @param convertMethodSource : parse函数字符串
+         * @param convertMethodName : parse函数name
+         * @param sourceParemKey : parse入参缓存Key
+         * @param sourceParamType : parse入参类型
+         * @param targetParemKey : parse函数返回值Key
+         * @param targetParamType : parse返回值类型
          *
          */
-        public Runner sourceParse(String convertFileSource, String convertMethodName, String sourceParemKey, String sourceParamType, String targetParemKey, String targetParamType) {
+        public Runner sourceParse(String convertMethodSource, String convertMethodName, String sourceParemKey, String sourceParamType, String targetParemKey, String targetParamType) {
             Parser parser = new Parser();
             try {
-                parser.parseValueVidStr(convertFileSource, convertMethodName, sourceParemKey, sourceParamType, targetParemKey, targetParamType);
+                parser.parseValueVidStr(convertMethodSource, convertMethodName, sourceParemKey, sourceParamType, targetParemKey, targetParamType);
             }
             catch (Exception ex) {
                 System.out.println(String.format("Init object failed"));
@@ -61,13 +80,18 @@ public class TestFlowManager {
         }
 
         /**
-         * 转化方法(java文件)
+         * 使用反射方法模式Parse
+         * @param convertFileName : 反射类名
+         * @param convertMethodName : 反射方法名
+         * @param sourceParemKey : parse入参缓存Key
+         * @param sourceParamType : parse入参类型
+         * @param targetParemKey : parse返回值key
          *
          */
-        public Runner reflectParse(String convertFileName, String convertMethodName, String sourceParemKey, String sourceDataParamType, String targetParemKey) {
+        public Runner reflectParse(String convertFileName, String convertMethodName, String sourceParemKey, String sourceParamType, String targetParemKey) {
             Parser parser = new Parser();
             try {
-                parser.parseValueViaFile(convertFileName, convertMethodName, sourceParemKey, sourceDataParamType, targetParemKey);
+                parser.parseValueViaFile(convertFileName, convertMethodName, sourceParemKey, sourceParamType, targetParemKey);
             }
             catch (Exception ex) {
                 System.out.println(String.format("Init object failed" + ex));
@@ -76,7 +100,11 @@ public class TestFlowManager {
         }
 
         /**
-         * 转化方法（重写方式）
+         * 使用子类重写方法模式Parse
+         * @param sourceParemType : parse入参缓存Key
+         * @param sourceParemKey : parse入参类型
+         * @param targeParemtKey : parse返回值key
+         * @param dataParser : dataParser重新类
          *
          */
         public Runner overrideParse(String sourceParemType, String sourceParemKey, String targeParemtKey, DataParser dataParser) {
@@ -102,7 +130,9 @@ public class TestFlowManager {
         }
 
         /**
-         * 对比实体
+         * 对比缓存中的Json
+         * @param expObj : 预期值缓存Key
+         * @param atlObj : 实际值缓存Key
          *
          */
         public Runner verify(String expObj, String atlObj) {
@@ -113,7 +143,12 @@ public class TestFlowManager {
         }
 
         /**
-         * 对比实体(包含对比List主键和实体中不对比属性)
+         * 对比两个相同类型的实体
+         * @param paramType : 实体类型
+         * @param expObj : 预期结果值缓存Key
+         * @param expObj : 实际结果值缓存Key
+         * @param expObj : 对比实体List主键
+         * @param atlObj : 实体中不对比的字段
          *
          */
         public Runner verify(String paramType, String expObj, String atlObj, String pkMapStr, String noCompareItemMapStr) {
@@ -129,13 +164,15 @@ public class TestFlowManager {
         }
 
         /**
-         * 对比下某属性实体
+         * 检验缓存中的Json某个key值
+         * @param atlObj : 实际结果值缓存Key
+         * @param JsonFilter : 定位Json的xpath
+         * @param expValue : 特定key的预期值
          *
          */
-        public Runner verify(String expObj, String JsonFilter, String expValue) {
-            Assert.assertEquals(ParamUtil.getMapValFromJson(BufferManager.getBufferByKey(expObj), JsonFilter), expValue);
+        public Runner verify(String atlObj, String JsonFilter, String expValue) {
+            Assert.assertEquals(ParamUtil.getMapValFromJson(BufferManager.getBufferByKey(atlObj), JsonFilter), expValue);
             return this;
         }
     }
 }
-
